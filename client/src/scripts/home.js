ec00d9d7da91fffb5ddcb17e682c6bad66967802
Eduardo Vazzoler <eduardotoril@gmail.com>
2024-05-21 15:50:38 -0300

    trying to get balance part 5

--- client/src/scripts/home.js
+++ client/src/scripts/home.js
@@ -1,28 +1,49 @@
 const PORT = 9998;
-
-// Get the username from the session storage
-document.addEventListener('DOMContentLoaded',  () => {
-    const id = sessionStorage.getItem('username');
-    console.log(id);
-})
-
 const jsonUrl = `http://localhost:${PORT}/api/banking-operation/balance/${id}`;
-// Fetch the JSON data
-fetch(jsonUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Display the balance
-    displayBalance(data.balance);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const username = sessionStorage.getItem('username');
+  
+  function formatData(data){
+    const formattedData = data.map(item => {
+      return {
+        id: item.id
+      };
+    });
+    return formattedData;
+  }
+
+  // Fetch the JSON data
+  try {
+    const response = await fetch(jsonUrl , {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.parse(formatData(data))
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      const errorData = await response.json();
+      console.error(errorData);
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+
 
 // Function to display the balance
 function displayBalance(balance) {
   // Find the element where you want to display the balance
   const balanceElement = document.getElementById('totalBalance');
-
   // Update the balance element with the retrieved balance
   balanceElement.textContent = `Balance: $${balance}`;
 }
