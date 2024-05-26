20d2a219d0ca9d8ba6910eb0d20bc3dac6c6f8df
Martin <martin_08_alba@hotmail.com>
2024-05-26 06:07:04 +0200

    feat: (backend) middleware & JWT

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
+import cors from 'cors'
 import { startServer } from './src/config.js'
 import userRoutes from './src/routes/user.routes.js'
 import operationRoutes from './src/routes/operation.routes.js'
-import cors from 'cors'
+import { authenticateJWT } from './src/middleware/protected-routes.middleware.js'
 
 const app = express()
 
@@ -22,6 +23,6 @@ app.get('/', async (req, res) => {
 })
 
 app.use('/api/users', userRoutes)
-app.use('/api/banking-operation', operationRoutes)
+app.use('/api/banking-operation', authenticateJWT, operationRoutes)
 
 startServer(app)
